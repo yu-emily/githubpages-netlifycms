41af7473545546229ab8e0a494be983fc33c4f90
Emily Yu <emily.yu@bellevuecollege.edu>
2021-01-24 13:25:17 -0800

    test netlify cms

--- public/render-page.js
+++ public/render-page.js
@@ -1,12 +1,12 @@
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory(require("/home/emily/Gatsby/TPF-Gatsby-Test/node_modules/lodash/merge.js"), require("react"), require("react-dom/server"));
+		module.exports = factory(require("/home/emily/Netlify/tpf-gatsby-netlify-cms/node_modules/lodash/merge.js"), require("react"), require("react-dom/server"));
 	else if(typeof define === 'function' && define.amd)
-		define("lib", ["/home/emily/Gatsby/TPF-Gatsby-Test/node_modules/lodash/merge.js", "react", "react-dom/server"], factory);
+		define("lib", ["/home/emily/Netlify/tpf-gatsby-netlify-cms/node_modules/lodash/merge.js", "react", "react-dom/server"], factory);
 	else if(typeof exports === 'object')
-		exports["lib"] = factory(require("/home/emily/Gatsby/TPF-Gatsby-Test/node_modules/lodash/merge.js"), require("react"), require("react-dom/server"));
+		exports["lib"] = factory(require("/home/emily/Netlify/tpf-gatsby-netlify-cms/node_modules/lodash/merge.js"), require("react"), require("react-dom/server"));
 	else
-		root["lib"] = factory(root["/home/emily/Gatsby/TPF-Gatsby-Test/node_modules/lodash/merge.js"], root["react"], root["react-dom/server"]);
+		root["lib"] = factory(root["/home/emily/Netlify/tpf-gatsby-netlify-cms/node_modules/lodash/merge.js"], root["react"], root["react-dom/server"]);
 })(this, function(__WEBPACK_EXTERNAL_MODULE_lodash_merge__, __WEBPACK_EXTERNAL_MODULE_react__, __WEBPACK_EXTERNAL_MODULE_react_dom_server__) {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
@@ -1643,9 +1643,9 @@ if (false) {} else {
 /***/ }),
 
 /***/ "lodash/merge":
-/*!**********************************************************************************!*\
-  !*** external "/home/emily/Gatsby/TPF-Gatsby-Test/node_modules/lodash/merge.js" ***!
-  \**********************************************************************************/
+/*!******************************************************************************************!*\
+  !*** external "/home/emily/Netlify/tpf-gatsby-netlify-cms/node_modules/lodash/merge.js" ***!
+  \******************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
